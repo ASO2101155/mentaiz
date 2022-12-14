07e752ba0e1e796be0d673045dc1f570f321acd7
monkarmtprg-Asou40 <119029016+monkarmtprg-Asou40@users.noreply.github.com>
2022-12-14 11:22:56 +0900

    Add files via upload

--- sample/login/login.php
+++ sample/login/login.php
@@ -60,6 +60,7 @@
 
 <link href='https://fonts.googleapis.com/css?family=Imperial Script' rel='stylesheet'>
 <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<title>login</title>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">            
     <div class="container-fluid">
     <a class="navbar-brand" style="font-family: 'Imperial Script'; font-size: 40px">Mentai's</a>
